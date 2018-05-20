00045dc4b1aa63617f884fffff8c63005e52dfcd
Sara Corak <saracorak0210@gmail.com>
2018-05-20 19:29:42 +0200

    Izmenjene neke greske

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 import {AuthService} from './services/auth.service';
 import { HeaderComponent } from './components/header/header.component';
+import { CategoryEntityService } from './services/category-entity.service';
+import { EntityService } from './services/entity-service.service';
+import { ServicesEntityService } from './services/services-entity.service';
+import { TypeEntityService } from './services/type-entity.service';
+import { HomeComponent } from './components/home/home.component';
+import { CategoryComponent } from './components/category/category.component';
+import { TypeComponent } from './components/type/type.component';
+import { AdditionalServiceComponent } from './components/additional-service/additional-service.component';
+import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
+import { AgentComponent } from './components/agent/agent.component';
+import { ImpressionComponent } from './components/impression/impression.component';
+import { UserComponent } from './components/user/user.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    CategoryComponent,
+    TypeComponent,
+    AdditionalServiceComponent,
+    AdminPanelComponent,
+    AgentComponent,
+    ImpressionComponent,
+    UserComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [AuthService],
+  providers: [AuthService, 
+              CategoryEntityService,
+              EntityService,
+              ServicesEntityService,
+              TypeEntityService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
