e0b372c6a43b873a1e04b6440e41bf5a640ed2ec
Sara Corak <saracorak0210@gmail.com>
2018-05-20 18:26:12 +0200

    Dodavanje komponenti i reorganizacija

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AdminPanelComponent } from '../components/admin-panel/admin-panel.component';
+import { HomeComponent } from '../components/home/home.component';
+import { CategoryComponent } from '../components/category/category.component';
+import { TypeComponent } from '../components/type/type.component';
+import { AdditionalServiceComponent } from '../components/additional-service/additional-service.component';
+import { UserComponent } from '../components/user/user.component';
+import { AgentComponent } from '../components/agent/agent.component';
+import { ImpressionComponent } from '../components/impression/impression.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'adminPanel', component: AdminPanelComponent, children: [
+    { path: 'category', component: CategoryComponent},
+    { path: 'type', component: TypeComponent},
+    { path: 'additionalService', component: AdditionalServiceComponent},
+    { path: 'users', component: UserComponent},
+    { path: 'agents', component: AgentComponent},
+    { path: 'impression', component: ImpressionComponent}
+  ]},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/components/impression/impression.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-impression',
+  templateUrl: './impression.component.html',
+  styleUrls: ['./impression.component.css']
+})
+export class ImpressionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
